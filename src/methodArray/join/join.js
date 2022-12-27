1d8172d9a438e886666304320582129177a2fd41
Vitor Melo da Silva <vmsvitor20@gmail.com>
2022-12-27 00:07:57 -0300

    feat: add example of join function

---
+++ src/methodArray/join/join.js
@@ -0,0 +1,12 @@
+/*
+ * Join é um método nativo do JavaScript para unir uma array em uma string, com um caracter
+ * ou não para unir os elementos da array.
+ */
+
+// * Array
+let array1 = [26, 12, 2022];
+
+// * join array in string
+let arrayJoin = array1.join('/');
+
+console.log('arrayJoin =>', arrayJoin);
