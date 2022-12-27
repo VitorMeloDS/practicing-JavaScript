141d10d2c1944eb7cbe83439c2368c440e280dff
Vitor Melo da Silva <vmsvitor20@gmail.com>
2022-12-27 00:08:16 -0300

    feat: add example of push function

---
+++ src/methodArray/push/push.js
@@ -0,0 +1,11 @@
+/*
+ * Push é um método nativo do JavaScript que serve para adicionarmos elementos no final do array.
+ */
+
+//* Array
+let array = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// Add elements in array
+array.push(8, 9, 10);
+
+console.log(array);
