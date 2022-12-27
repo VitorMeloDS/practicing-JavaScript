50d827c90330435c5e2c1c55b879a48381af7968
Vitor Melo da Silva <vmsvitor20@gmail.com>
2022-12-27 00:07:22 -0300

    feat: add example of concat function

---
+++ src/methodArray/concat/concat.js
@@ -0,0 +1,12 @@
+/*
+ * Concat é um método nativo do JavaScript para unir duas array ou mais array em uma.
+ */
+
+// * Arrays
+let array1 = [1, 2, 3, 4, 5, 6];
+let array2 = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+// * Concat tow arrays in one
+let arrayConcat = array1.concat(array2);
+
+console.log('arrayConcat =>', arrayConcat);
